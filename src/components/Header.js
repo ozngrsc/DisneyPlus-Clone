66ea9495d0234f97011db8ff5e8c8f6396293288
ozngrsc <ozangursucu@hotmail.com>
2022-07-18 15:58:23 +0300

    added Login Button in Header Component an given style

--- src/components/Header.js
+++ src/components/Header.js
@@ -12,7 +12,28 @@ const Header = (props) => {
           <img src="/images/home-icon.svg" alt="HOME" />
           <span>HOME</span>
         </a>
+        <a href="/">
+          <img src="/images/search-icon.svg" alt="SEARCH" />
+          <span>SEARCH</span>
+        </a>
+        <a href="/">
+          <img src="/images/watchlist-icon.svg" alt="WATCHLIST" />
+          <span>WATCHLIST</span>
+        </a>
+        <a href="/">
+          <img src="/images/original-icon.svg" alt="ORIGINALS" />
+          <span>ORIGINALS</span>
+        </a>
+        <a href="/">
+          <img src="/images/movie-icon.svg" alt="MOVIES" />
+          <span>MOVIES</span>
+        </a>
+        <a href="/">
+          <img src="/images/series-icon.svg" alt="SERIES" />
+          <span>SERIES</span>
+        </a>
       </NavMenu>
+      <Login>LOGIN</Login>
     </Nav>
   );
 };
@@ -108,6 +129,22 @@ const NavMenu = styled.div`
 
   @media (max-width: 768px) {
     display: none;
+  }
+`;
+
+const Login = styled.a`
+  background-color: rgba(0, 0, 0, 0.6);
+  padding: 8px 16px;
+  text-transform: uppercase;
+  letter-spacing: 1.5px;
+  border: 1px solid #f9f9f9;
+  border-radius: 4px;
+  transition: all 0.2s ease 0s;
+
+  &:hover {
+    background-color: #f9f9f9;
+    color: #000;
+    border-color: transparent;
   }
 `;
 
