5b8774090bc68aafa20797df6bf5934119e6d421
ozngrsc <ozangursucu@hotmail.com>
2022-07-18 14:59:03 +0300

    created Header component and given style

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Login from "./components/Login";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="app">
       <Router>
+        <Header />
         <Routes>
           <Route exact path="/" element={<Login />} />
         </Routes>
---
+++ src/components/Header.js
@@ -0,0 +1,61 @@
+import styled from "styled-components";
+import React from "react";
+
+const Header = (props) => {
+  return (
+    <Nav>
+      <Logo>
+        <img src="/images/logo.svg" alt="Disney+" />
+      </Logo>
+      <NavMenu>Menu</NavMenu>
+    </Nav>
+  );
+};
+
+const Nav = styled.nav`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  height: 70px;
+  background-color: #090b13;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 36px;
+  letter-spacing: 16px;
+  z-index: 3;
+`;
+
+const Logo = styled.a`
+  padding: 0;
+  width: 80px;
+  margin-top: 4px;
+  max-height: 70px;
+  font-size: 0;
+  display: inline-block;
+
+  img {
+    display: block;
+    width: 100%;
+  }
+`;
+
+const NavMenu = styled.div`
+  display: flex;
+  align-items: center;
+  flex-flow: row nowrap;
+  height: 100%;
+  justify-content: flex-end;
+  margin: 0px;
+  padding: 0px;
+  position: relative;
+  margin-right: auto;
+  margin-left: 25px;
+
+  @media (max-width: 768px) {
+    display: none;
+  }
+`;
+
+export default Header;
