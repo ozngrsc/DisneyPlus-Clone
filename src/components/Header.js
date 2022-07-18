5669f94858fdb055bb3961db28efbea663e27524
ozngrsc <ozangursucu@hotmail.com>
2022-07-18 15:30:36 +0300

    added NavMenu in Header and given style and hover

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,12 @@ const Header = (props) => {
       <Logo>
         <img src="/images/logo.svg" alt="Disney+" />
       </Logo>
-      <NavMenu>Menu</NavMenu>
+      <NavMenu>
+        <a href="/home">
+          <img src="/images/home-icon.svg" alt="HOME" />
+          <span>HOME</span>
+        </a>
+      </NavMenu>
     </Nav>
   );
 };
@@ -52,6 +57,54 @@ const NavMenu = styled.div`
   position: relative;
   margin-right: auto;
   margin-left: 25px;
+
+  a {
+    display: flex;
+    align-items: center;
+    padding: 0 12px;
+
+    img {
+      height: 20px;
+      min-width: 20px;
+      width: 20px;
+      z-index: auto;
+    }
+
+    span {
+      color: rgb(249, 249, 249);
+      font-size: 13px;
+      letter-spacing: 1.42px;
+      line-height: 1.08;
+      padding: 2px 0px;
+      white-space: nowrap;
+      position: relative;
+
+      &:before {
+        background-color: rgb(249, 249, 249);
+        border-radius: 0px 0px 4px 4px;
+        bottom: -6px;
+        content: "";
+        height: 2px;
+        left: 0px;
+        opacity: 0;
+        position: absolute;
+        right: 0px;
+        transform-origin: left center;
+        transform: scaleX(0);
+        transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
+        visibility: hidden;
+        width: auto;
+      }
+    }
+
+    &:hover {
+      span:before {
+        transform: scaleX(1);
+        visibility: visible;
+        opacity: 1 !important;
+      }
+    }
+  }
 
   @media (max-width: 768px) {
     display: none;
