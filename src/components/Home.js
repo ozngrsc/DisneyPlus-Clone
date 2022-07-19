bd38dd9b9378b02928c0784b71677be200be9aca
ozngrsc <ozangursucu@hotmail.com>
2022-07-19 23:34:34 +0300

    created Container in Home page and edited backgroundImg

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,26 @@
 import styled from "styled-components";
 
 const Home = (props) => {
-  return <div>Home</div>;
+  return <Container>Home</Container>;
 };
+
+const Container = styled.main`
+  position: relative;
+  min-height: calc(100vh - 250px);
+  overflow-x: hidden;
+  display: block;
+  top: 72px;
+  padding: 0 calc(3.5vw + 5px);
+
+  &:after {
+    background: url("/images/home-background.png") center center / cover
+      no-repeat fixed;
+    content: "";
+    position: absolute;
+    inset: 0px;
+    opacity: 1;
+    z-index: -1;
+  }
+`;
 
 export default Home;
