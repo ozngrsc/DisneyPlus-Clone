ef586980ab7bb5d1c41015c8fa362fc242833d15
ozngrsc <ozangursucu@hotmail.com>
2022-07-20 12:49:33 +0300

    added slider img and given style

--- src/components/ImgSlider.js
+++ src/components/ImgSlider.js
@@ -15,15 +15,26 @@ const ImgSlider = (props) => {
 
   return (
     <Carousel {...settings}>
-      <div>
-        <h3>1</h3>
-      </div>
-      <div>
-        <h3>2</h3>
-      </div>
-      <div>
-        <h3>3</h3>
-      </div>
+      <Wrap>
+        <a href="">
+          <img src="/images/slider-badging.jpg" alt="" />
+        </a>
+      </Wrap>
+      <Wrap>
+        <a href="">
+          <img src="/images/slider-scale.jpg" alt="" />
+        </a>
+      </Wrap>
+      <Wrap>
+        <a href="">
+          <img src="/images/slider-badag.jpg" alt="" />
+        </a>
+      </Wrap>
+      <Wrap>
+        <a href="">
+          <img src="/images/slider-scales.jpg" alt="" />
+        </a>
+      </Wrap>
     </Carousel>
   );
 };
@@ -64,6 +75,33 @@ const Carousel = styled(Slider)`
 
   .slick-next {
     right: -75px;
+  }
+`;
+
+const Wrap = styled.div`
+  border-radius: 4px;
+  cursor: pointer;
+  position: relative;
+
+  a {
+    border-radius: 4px;
+    box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px,
+      rgb(0 0 0 / 73%) 0px 16px 10px -10px;
+    cursor: pointer;
+    display: block;
+    position: relative;
+    padding: 4px;
+
+    img {
+      width: 100%;
+      height: 100%;
+    }
+
+    &:hover {
+      padding: 0;
+      border: 4px solid rgba(249, 249, 249, 0.8);
+      transition-duration: 300ms;
+    }
   }
 `;
 
